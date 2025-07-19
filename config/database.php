3307ce3b8a0b55d463f6d746343526943c9e7d27
debayolo1 <debayolo968@gmail.com>
2025-07-19 17:12:53 +0200

    fourth last

--- config/database.php
+++ config/database.php
@@ -7,9 +7,7 @@ return [
     | Default Database Connection Name
     |--------------------------------------------------------------------------
     |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
+    | Set your default connection via .env DB_CONNECTION (e.g. pgsql for PostgreSQL)
     |
     */
 
@@ -20,14 +18,8 @@ return [
     | Database Connections
     |--------------------------------------------------------------------------
     |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
+    | Laravel supports several database drivers. Make sure your chosen
+    | driver is installed on your machine or container.
     |
     */
 
@@ -69,19 +61,17 @@ return [
             'engine' => null,
         ],
 
-
-
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', '127.0.0.1'),       // Render DB hostname set in .env
+            'port' => env('DB_PORT', '5432'),            // Render PostgreSQL port
+            'database' => env('DB_DATABASE', 'forge'),   // Render database name
+            'username' => env('DB_USERNAME', 'forge'),   // Render DB user
+            'password' => env('DB_PASSWORD', ''),        // Render DB password
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'prefer',                        // Enable SSL if Render requires
         ],
 
         'sqlsrv' => [
@@ -102,9 +92,7 @@ return [
     | Migration Repository Table
     |--------------------------------------------------------------------------
     |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
+    | This table tracks migrations that have been run.
     |
     */
 
@@ -114,11 +102,6 @@ return [
     |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
     */
 
     'redis' => [
